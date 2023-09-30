a32e1be0671c95aee18fd079bd246226314dd763
Nicholas Tay <nick@windblume.net>
2023-09-30 21:04:33 +1000

    Add GitHub CI Windows x64 build

--- buildresource.sh
+++ buildresource.sh
@@ -5,4 +5,7 @@
 
 set -xe
 
-windres -i ortlinde.rc -O coff -o ortlinde.syso
+WINDRES=windres
+# (on linux crosscompile)
+command -v x86_64-w64-mingw32-windres >/dev/null && WINDRES=x86_64-w64-mingw32-windres
+$WINDRES -i ortlinde.rc -O coff -o ortlinde.syso
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/nicholastay/ortlinde
 
 go 1.21.1
 
-require golang.org/x/sys v0.12.0 // indirect
+require golang.org/x/sys v0.12.0
